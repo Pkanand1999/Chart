2bf78ec2828008dbcbf190cfa04cd5001753786f
Pkanand1999 <singhisking810267@gmail.com>
2023-07-13 20:46:55 +0530

    chart

--- frontend/vite-project/src/components/LineChart.jsx
+++ frontend/vite-project/src/components/LineChart.jsx
@@ -1,7 +1,8 @@
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS,CategoryScale,LinearScale, PointElement, LineElement, Title, Tooltip, Legend, } from 'chart.js';
+import { Chart } from 'react-google-charts';
+import Pie from './Pie';
+// import { Chart as ChartJS,CategoryScale,LinearScale, PointElement, LineElement, Title, Tooltip, Legend, } from 'chart.js';
 
-ChartJS.register(CategoryScale,LinearScale,PointElement,LineElement,Title, Tooltip, Legend );
+// ChartJS.register(CategoryScale,LinearScale,PointElement,LineElement,Title, Tooltip, Legend );
 
 
 
@@ -9,12 +10,14 @@ function LineChart() {
     
   return (
     <>{/* chart  */}
-    <div className='h-[38%] '>
-        <div style={{height:"100% ",width:"50% "}} >
-        <Line  options={options} data={data} />
-        </div>
+    <div className='h-[38%] rounded-2xl'>
+        {/* <div style={{height:"100% ",width:"50% "}} > */}
+        <Chart  chartType="LineChart" height="100%" className='p-6 rounded-xl' options={options} data={data} />
+        {/* </div> */}
     </div>
-    <div>sdflsdf</div>
+    <div className=' w-[100%] h-2/6'>
+<Pie/>
+    </div>
     {/* chart  */}
     </>
   )
@@ -22,57 +25,76 @@ function LineChart() {
 
 export default LineChart
 
+const data = [
+    ["week", "Guest", "User"],
+    ["",0,0],
+    ["week1", 366, 133],
+    ["Week2", 211, 244],
+    ["week3", 388, 199],
+    ["week4", 455, 400],
+  ];
+  
+   const options = {
+    title: "Activity",
+    series: {
+        0: { curveType: "function",color:'rgb(233,160,160)' },
+        1: { curveType: "function",color:"rgb(155,221,124)" },
+      },
+    legend: { position: "top" },
+  };
 
- const options = {
-    resposive: false,
-    maintainAspectRatio: 10/3,
-    plugins: {
-      legend: {
-        position: 'top' ,
-      },
-      title: {
-        display: true,
-        text: 'Activity',
-        font:{size:25}
-      },
-    },
-    scales:{
-        x:{
-            grid:{
-                display:false,
-            },
-        },
-        y:{
-            grid:{
-                display:true
-            }
-        },
-    },
-    devicePixelRatio: 80/10
-  };
+//  const options = {
+//     resposive: false,
+//     maintainAspectRatio: 10/3,
+//     plugins: {
+//       legend: {
+//         position: 'top' ,
+//       },
+//       title: {
+//         display: true,
+//         text: 'Activity',
+//         font:{size:25}
+//       },
+//     },
+//     scales:{
+//         x:{
+//             grid:{
+//                 display:false,
+//             },
+//             height:"100%",
+//         },
+//         y:{
+//             grid:{
+//                 display:true
+//             },
+//             width:"100%",
+//         },
+//     },
+//     devicePixelRatio: 80/10
+//   };
   
-  const labels = ['1st Week', '2nd Week', '3rd Week', '4th Week'];
+//   const labels = ['1st Week', '2nd Week', '3rd Week', '4th Week'];
   
-   const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Guest',
-        data: [400,234,334,123],
-        borderColor: 'rgb(233,160,160)',
-        backgroundColor: 'rgb(233,160,160)',
-        tension:0.5,
-        borderWidth:2,
-        pointRadius:0,
-      },
-      {
-        label: 'User',
-        data: [134,346,257,467],
-        borderColor: 'rgb(155,221,124)',
-        backgroundColor: 'rgb(155,221,124)',
-        tension:0.5,
-        borderWidth:2,
-        pointRadius:0,
-      },
-    ],
-  };
+//    const data = {
+//     labels,
+//     datasets: [
+//       {
+//         label: 'Guest',
+//         data: [400,234,334,123],
+//         borderColor: 'rgb(233,160,160)',
+//         backgroundColor: 'rgb(233,160,160)',
+//         tension:0.5,
+//         borderWidth:2,
+//         pointRadius:0,
+//       },
+//       {
+//         label: 'User',
+//         data: [134,346,257,467],
+//         borderColor: 'rgb(155,221,124)',
+//         backgroundColor: 'rgb(155,221,124)',
+//         tension:0.5,
+//         borderWidth:2,
+//         pointRadius:0,
+//       },
+//     ],
+//   };
---
+++ frontend/vite-project/src/components/Pie.jsx
@@ -0,0 +1,27 @@
+import { Chart } from 'react-google-charts';
+
+function Pie() {
+  return (
+    <>
+    <Chart chartType="PieChart"
+      data={data}
+      options={options}
+      className='p-6 rounded-xl'
+      width={"70%"}
+      height={"100%"}/>
+    </>
+  )
+}
+
+export default Pie
+
+export const data = [
+    ["Task", "Hours per Day"],
+    ["Basic Tees", 2],
+    ["Costum Short Pants", 2],
+    ["Super Hoodies", 7],
+  ];
+  
+  export const options = {
+    title: "Top Products",
+  };
