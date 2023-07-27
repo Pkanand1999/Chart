aa881ae575b61df0a03acee65f98ab1678514f2f
Pkanand1999 <singhisking810267@gmail.com>
2023-07-27 22:36:32 +0530

    chart update

--- frontend/vite-project/src/pages/Auth.jsx
+++ frontend/vite-project/src/pages/Auth.jsx
@@ -11,14 +11,13 @@ setSignup(false)
 function changetosignup(){
 setSignup(true)
 }
-console.log(`${import.meta.env.VITE_AUTH_URL}`)
 
 
   return (
     <>
     <div className='flex h-screen justify-between'>
     <div className='bg-black w-2/5 flex justify-center items-center'>
-        <h1 className='text-6xl font-bold text-white'>Board.</h1>
+        <h1 className='text-6xl font-bold text-white'></h1>
     </div>
     <div className='w-3/5 flex justify-center items-center bg-gray-300'>
       {signup && <Signup changetologin={changetologin}/>}
